3a290dabe7dbad5a1425f059b9581877ce8bfbc2
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-09-22 21:16:02 +0200

    registration component with form created

---
+++ src/components/Registration/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+
+const Registration = () => {
+
+    let inputType = "";
+    let defaultUsername = "";
+
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirm: '',
+     }) 
+
+     function register (event) {
+       /*  if(user.username==''){
+            const usernameDefault = "jirinka";
+            setUser({...user, username: usernameDefault})
+        } */
+        event.preventDefault()
+        
+        console.log(user);
+     }
+    /*  function handleChange (e, field) { 
+        setUser({...user, field: e.target.value})
+     } */
+
+    return(
+        <>
+        <form>
+            <input type="email" placeholder="Email address"
+                onChange={(e) => setUser({...user, email: e.target.value})} required ></input><br></br>
+            <input  placeholder="User Name"
+                onChange={(e) => setUser({...user, username: e.target.value})} ></input><br></br>
+            <input type="password" placeholder="Password"
+                onChange={(e) => setUser({...user, password: e.target.value})} ></input><br></br>
+            <input type="password" placeholder="Confirm Password"
+                onChange={(e) => setUser({...user, passwordConfirm: e.target.value})}></input><br></br>
+
+            <button type="submit" onClick={register}>Register</button>
+        </form>
+
+
+        </>
+    )
+
+}
+
+export default Registration;
