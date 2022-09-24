93548153294cb99fd96a3338fdaefc9a00f994ce
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-09-24 16:55:08 +0200

    add default username

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -3,9 +3,6 @@ import { useState } from "react";
 
 const Registration = () => {
 
-    let inputType = "";
-    let defaultUsername = "";
-
     const [user, setUser] = useState({
         username: '',
         email: '',
@@ -13,13 +10,14 @@ const Registration = () => {
         passwordConfirm: '',
      }) 
 
-     function register (event) {
-        
+     function fillUsername(){       
         if(!user.username){
-            const usernameDefault = "jirinka";
-            setUser({...user, username: "jirinka"})
+            let prefilledUsername = user.email.split("@")[0];
+            setUser({...user, username: prefilledUsername})
         } 
+     }
 
+     function register (event) {
         event.preventDefault()    
         console.log(user);
      }
@@ -31,17 +29,16 @@ const Registration = () => {
         <>
         <form>
             <input type="email" placeholder="Email address"
-                onChange={(e) => {setUser({...user, email: e.target.value}); console.log(user.email)}} required ></input><br></br>
-            <input  placeholder="User Name"
+                onChange={(e) => {setUser({...user, email: e.target.value })}} required ></input><br></br>
+            <input  placeholder="User Name" value={user.username}
                 onChange={(e) => setUser({...user, username: e.target.value})} ></input><br></br>
-            <input type="password" placeholder="Password"
+            <input type="password" placeholder="Password" required
                 onChange={(e) => setUser({...user, password: e.target.value})} ></input><br></br>
-            <input type="password" placeholder="Confirm Password"
+            <input type="password" placeholder="Confirm Password" required
                 onChange={(e) => setUser({...user, passwordConfirm: e.target.value})}></input><br></br>
 
-            <button type="submit" onClick={register}>Register</button>
+            <button type="submit" onClick={register} onMouseOver={fillUsername}>Register</button>
         </form>
-
         </>
     )
 }
